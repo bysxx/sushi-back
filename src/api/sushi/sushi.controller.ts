82afb0f3b10f5f96541ae1c63dbc999d3d3b8247
bysxx <0415white@gmail.com>
2023-08-02 16:51:00 +0900

    feat: update api (get, delete)

--- src/api/sushi/sushi.controller.ts
+++ src/api/sushi/sushi.controller.ts
@@ -1,15 +1,32 @@
-import { Body, JsonController, Post } from "routing-controllers";
+import {
+	Body, Delete, Get, JsonController, Param, Post,
+} from "routing-controllers";
 import { CreateSushiDTO } from "./dto/create-sushi.dto";
 import { SushiService } from "./sushi.service";
 
 @JsonController("/sushi")
 export class SushiController {
 	constructor(
-		private readonly sushiService: SushiService
+    private readonly sushiService: SushiService,
 	) { }
 
-	@Post()
-	public async create(@Body() body: CreateSushiDTO) {
-		return await this.sushiService.create(body);
+  @Get()
+	public async get() {
+		return this.sushiService.getAllSushi();
 	}
-}	
+
+  @Get("/:id")
+  public async getOne(@Param("id") id: number) {
+  	return this.sushiService.getSushiById(id);
+  }
+
+  @Post()
+  public async create(@Body() body: CreateSushiDTO) {
+  	return this.sushiService.create(body);
+  }
+
+  @Delete("/:id")
+  public async deleteOne(@Param("id") id: number) {
+  	return this.sushiService.deleteSushiById(id);
+  }
+}
--- src/api/sushi/sushi.service.ts
+++ src/api/sushi/sushi.service.ts
@@ -7,9 +7,33 @@ import { CreateSushiDTO } from "./dto/create-sushi.dto";
 @Service()
 export class SushiService {
 	constructor(
-		@InjectRepository()
-		private readonly sushiRepo: SushiRepository
+    @InjectRepository()
+    private readonly sushiRepo: SushiRepository,
 	) {}
+
+	public async getAllSushi() {
+		try {
+			return await this.sushiRepo.find();
+		} catch (e) {
+			throw new BaseException(400, "get list error", e);
+		}
+	}
+
+	public async getSushiById(id: number) {
+		try {
+			return await this.sushiRepo.findOne(id);
+		} catch (e) {
+			throw new BaseException(400, "create error", e);
+		}
+	}
+
+	public async deleteSushiById(id: number) {
+		try {
+			return await this.sushiRepo.delete(id);
+		} catch (e) {
+			throw new BaseException(400, "create error", e);
+		}
+	}
 
 	public async create(createSushiDTO: CreateSushiDTO) {
 		try {
